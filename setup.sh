ca850b99ac4ec454a59d2a79bdaff0a8d593e8d6
Yoshihide Shirai <yoshihide.shirai@gmail.com>
2020-11-03 17:59:45 +0900

    neovim init.vim

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DOT_FILES=()
+DOT_CONFIG_FILES=(nvim)
+
+for file in ${DOT_FILES[@]}
+do
+	ln -s $HOME/dotfiles/$file $HOME/$file
+done
+
+for file in ${DOT_CONFIG_FILES[@]}
+do
+	rm -rf $HOME/.config/$file
+	ln -s $HOME/dotfiles/.config/$file $HOME/.config/$file
+done
